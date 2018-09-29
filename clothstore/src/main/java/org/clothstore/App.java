ce3528062d2ae3fb756788ef456e9a6332e3345c
andychenn <ctf531buy@163.com>
2018-09-29 17:39:06 +0800

    初次版本提交

---
+++ clothstore/src/main/java/org/clothstore/App.java
@@ -0,0 +1,13 @@
+package org.clothstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
